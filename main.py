3da6f0d9a46391e70b6741272afe590f6fcfca2a
Jiří <gresjirka@gmail.com>
2025-02-12 12:50:36 +0100

    GUI

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from math import pi, sin
 from os.path import basename, splitext
 import tkinter as tk
+import matplotlib.pyplot as plt
+import numpy as np
 
 # from tkinter import ttk
 
@@ -43,14 +46,41 @@ class Application(tk.Tk):
 
     def __init__(self):
         super().__init__(className=self.name)
+
+        self.func = tk.StringVar()
+
         self.title(self.name)
-        self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
-        self.lbl.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
+
+        self.func_frame = tk.LabelFrame(self, text="Graf z funkce")
+        self.sin = tk.Radiobutton(
+            self.func_frame, text="sin", variable=self.func, value="sin"
+        )
+        self.log = tk.Radiobutton(
+            self.func_frame, text="log", variable=self.func, value="log"
+        )
+        self.exp = tk.Radiobutton(
+            self.func_frame, text="exp", variable=self.func, value="exp"
+        )
+        self.start = MyEntry(self.func_frame)
+        self.end = MyEntry(self.func_frame)
+
+        self.sin.grid(row=0)
+        self.log.grid(row=1)
+        self.exp.grid(row=2)
+        self.start.grid(row=0, column=1)
+        self.end.grid(row=1, column=1)
+        self.func_frame.grid()
+
+        self.file_frame = tk.LabelFrame(self, text="Graf ze souboru")
+        self.file_name = tk.Entry(self.file_frame)
+        self.file_select = tk.Button(self.file_frame, text="Vybrat soubor")
+
+        self.file_name.grid()
+        self.file_select.grid(row=1)
+        self.file_frame.grid(row=1)
+
+    def _draw(self):
+        self.draw_frame = tk.LabelFrame(self, text="Vykreslit graf")
 
     def about(self):
         window = About(self)
@@ -59,6 +89,13 @@ class Application(tk.Tk):
     def quit(self, event=None):
         super().quit()
 
+
+x = np.linspace(0, 2 * np.pi, 200)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
 
 app = Application()
 app.mainloop()
